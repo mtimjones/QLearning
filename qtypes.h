6d9184d4a14116f5e5fc7a69d7ee3015a393b0bd
M. Tim Jones <mtj@mtjones.com>
2017-09-09 11:47:00 -0600

    Added comments.

--- qtypes.h
+++ qtypes.h
@@ -1,12 +1,12 @@
 
 #include <stdlib.h>
 
-#define X_MAX           20      /* Grid World Size */
+#define X_MAX           20
 #define Y_MAX           20
 
-#define MAX_EPOCHS	200000L /* Number of epochs to run */
+#define MAX_EPOCHS	200000L // Number of epochs to run
 
-#define MAX_ACTIONS     4       /* Actions define 4 possible directions */
+#define MAX_ACTIONS     4       // Actions define 4 possible directions
 
 typedef struct {
    int y;
@@ -19,11 +19,11 @@ typedef struct {
    double QMax;
 } stateAction_t;
 
-#define LEARNING_RATE	0.3	/* alpha */
-#define DISCOUNT_RATE   0.8	/* gamma */
+#define LEARNING_RATE	0.3	// alpha
+#define DISCOUNT_RATE   0.8	// gamma
 
-#define EXPLOIT         0   /* Choose best Q */
-#define EXPLORE         1   /* Probabilistically choose best Q */
+#define EXPLOIT         0   // Choose best Q
+#define EXPLORE         1   // Probabilistically choose best Q
 
 #define getSRand()      ( ( double ) rand( ) / ( double ) RAND_MAX )
 #define getRand(x)      ( int )( ( double )( x ) * rand( ) / ( RAND_MAX+1.0 ) )
